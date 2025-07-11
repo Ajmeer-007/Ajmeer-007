0aa35cdc2dd73232ab695cff79f3ef8de62fb3cd
Mohamed Ajmeer M <mohamedajmeer20051103@gmail.com>
2025-07-11 19:29:41 +0530

    Update Move All Zeros to end.py

--- 160 Days of coding Chellenge/Move All Zeros to end.py
+++ 160 Days of coding Chellenge/Move All Zeros to end.py
@@ -1,20 +1,11 @@
+
 class Solution:
-    def getSecondLargest(self, arr):
-        # Code Here
-        arr.sort()
-        #maxi = max(arr)
-        n = len(arr)
-        cur_max = arr[0]
-        
-        if n == 1:
-            return -1
-            
-        for i in range(n):
-            if arr[i]>cur_max and arr[i] != arr[n-1]:
-                cur_max = arr[i]
-                
-        if (cur_max != arr[n-1]):
-            return cur_max
-        else:
-            return -1
-                
+	def pushZerosToEnd(self,arr):
+    	# code here
+    	
+    	left = 0
+    	for right in range(len(arr)):
+    	    if arr[right]!=0:
+    	        arr[right],arr[left] = arr[left],arr[right]
+    	        left+=1
+    	return arr

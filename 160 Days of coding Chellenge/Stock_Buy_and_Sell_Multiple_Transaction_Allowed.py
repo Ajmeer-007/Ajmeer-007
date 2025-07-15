1c56b0354daa73eef02b460896ae9b8db810fae7
Mohamed Ajmeer M <mohamedajmeer20051103@gmail.com>
2025-07-15 19:33:57 +0530

    Stock_Buy_and_Sell_Multiple_Transaction_Allowed.py

---
+++ 160 Days of coding Chellenge/Stock_Buy_and_Sell_Multiple_Transaction_Allowed.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maximumProfit(self, prices) -> int:
+        # code here
+        n = len(prices)
+        profit = 0
+        for i in range(1,n):
+            if prices[i]>prices[i-1]:
+                profit+=prices[i]-prices[i-1]
+        return profit
+           
